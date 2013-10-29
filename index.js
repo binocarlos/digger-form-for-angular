62a59e9176fe441cc81ce8ec5d9cc9cb5da37042
Kai Davenport <kaiyadavenport@gmail.com>
2013-10-29 23:36:05 +0000

    added component to here and sorted out the variable flow in the field-renderer and list -> component

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ var templates = {
 
 angular
   .module('digger.form', [
-    
+    'digger',
+    'digger.bootstrap'
   ])
 
   .filter('fieldtitle', function(){
@@ -163,6 +164,70 @@ angular
     }
   })
 
+
+  /*
+  
+    the directive that triggers us loading the component remotely and injecting / compiling it when done
+    
+  */
+  .directive('diggerComponent', function(DiggerComponentLoader, $compile){
+    return {
+      restrict:'EA',
+      scope:{
+        name:'=',
+        field:'=',
+        container:'=', 
+        model:'=',
+        fieldname:'=',
+        readonly:'='
+      },
+      replace:true,
+      template:'<div></div>',
+      controller:function($scope){
+
+        // we load the component from the server
+        // once it has done - require the component (it has registered via the script load)
+        $scope.$watch('name', function(name){
+          if(!name){
+            return;
+          }
+
+          if($digger.config.debug){
+            console.log('-------------------------------------------');
+            console.log('compiling component: ' + name);
+          }
+          DiggerComponentLoader.load(name).then(function(html){
+            if($digger.config.debug){
+              console.log('-------------------------------------------');
+              console.log(name + ' LOADED');
+              console.dir(html);
+            }
+
+            $scope.component_html = html;
+            
+          }, function(error){
+            if($digger.config.debug){
+              console.log('-------------------------------------------');
+              console.log('component error!');
+              console.dir(error);
+            }
+          })
+        })
+
+        
+
+      },
+      link:function($scope, elem, $attrs){
+
+        // this is changed once we have loaded the remote component
+        $scope.$watch('component_html', function(html){
+          var widget = $compile(html)($scope);
+          elem.html('');
+          elem.append(widget);
+        })
+      }
+    }
+  })
 
   .directive('diggerFieldRender', function($compile, $diggerFieldTypes){
 
@@ -464,7 +529,6 @@ angular
           if($scope.field.list && !$scope.model[$scope.field.name]){
             $scope.model[$scope.field.name] = [];
           }
-
         }
 
 
