ab2aa1adbf14777abcc4cc61b77d596cd2cf462a
Kai Davenport <kaiyadavenport@gmail.com>
2014-01-21 15:57:43 +0000

    remove components - return name as module

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ var templates = {
 
 angular
   .module('digger.form', [
-    'digger',
-    'digger.bootstrap'
+    require('digger-utils-for-angular')
   ])
 
   .filter('fieldtitle', function(){
@@ -138,9 +137,9 @@ angular
       template:true,
       checkbox:true,
       radio:true,
-      select:true,
+      select:true/*,
       diggerurl:'binocarlos/digger-url-component',
-      file:'binocarlos/file-uploader'
+      file:'binocarlos/file-uploader'*/
     }
 
     return {
@@ -185,72 +184,6 @@ angular
           fieldname:fieldname,
           model:container.get(0)
         }
-      }
-    }
-  })
-
-  /*
-  
-    the directive that triggers us loading the component remotely and injecting / compiling it when done
-    
-  */
-  .directive('diggerComponent', function(DiggerComponentLoader, $compile){
-    return {
-      restrict:'EA',
-      scope:{
-        name:'=',
-        field:'=',
-        container:'=', 
-        model:'=',
-        fieldname:'=',
-        readonly:'='
-      },
-      replace:true,
-      template:'<div></div>',
-      controller:function($scope){
-
-        // we load the component from the server
-        // once it has done - require the component (it has registered via the script load)
-        $scope.$watch('name', function(name){
-
-          if(!name){
-            return;
-          }
-
-
-          if($digger.config.debug){
-            console.log('-------------------------------------------');
-            console.log('compiling component: ' + name);
-          }
-          DiggerComponentLoader.load(name).then(function(html){
-            if($digger.config.debug){
-              console.log('-------------------------------------------');
-              console.log(name + ' LOADED');
-              console.dir(html);
-            }
-
-            $scope.component_html = html;
-            
-          }, function(error){
-            if($digger.config.debug){
-              console.log('-------------------------------------------');
-              console.log('component error!');
-              console.dir(error);
-            }
-          })
-        })
-
-        
-
-      },
-      link:function($scope, elem, $attrs){
-
-        // this is changed once we have loaded the remote component
-        $scope.$watch('component_html', function(html){
-          var widget = $compile(html)($scope);
-          elem.html('');
-          elem.append(widget);
-        })
       }
     }
   })
@@ -363,7 +296,7 @@ angular
             if they have registered a custom template then use that!
             
           */
-          var template = $digger.template.get(fieldtype);
+          var template = $digger.blueprint.get_template(fieldtype);
 
           if(template){
             $scope.fieldtype = 'template';
